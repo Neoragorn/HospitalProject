4e858971e69a06dad6c97949f2de06bb8bab1050
Neoragorn <casiersofian@gmail.com>
2017-12-25 19:47:54 +0100

    third

---
+++ src/main/java/Beans/HomeBean.java
@@ -0,0 +1,20 @@
+package Beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class HomeBean {
+
+	public HomeBean() {
+
+	}
+
+	public void init()
+	{
+		FacesContext context = FacesContext.getCurrentInstance();
+        
+        context.addMessage(null, new FacesMessage("Login Successful : Welcome to PatientMedicManagement",  "Welcome to PatientMedicManagement" ));
+	}
+}
